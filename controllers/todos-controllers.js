a39fa2c6a2006743089faaa39d91d88dbb5006ed
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-08-03 13:16:47 +0100

    made corrections

--- controllers/todos-controllers.js
+++ controllers/todos-controllers.js
@@ -1,95 +1,118 @@
 const Todo = require('../models/todo');
 const moment = require('moment');
 const { validationResult } = require('express-validator');
-const HttpError = require('../models/http-error');
+
 
 //Creat a new Todo
 const createTodo = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(404).json({message: 'Invalid Inputs passed, check your data'})
+        return res.status(404).json({ message: 'name must be entered' })
     }
     const createdTodo = new Todo({
         name: req.body.name,
-        time: moment.utc(req.body.time, 'YYYY-MM-DD hh:mm:ss a'),
+        reminderAt: req.body.reminderAt ? moment.utc(req.body.reminderAt, 'YYYY-MM-DD hh:mm:ss a') : null,
         reminder: req.body.reminder,
         isCompleted: req.body.isCompleted
     });
     try {
         await createdTodo.save();
     } catch (err) {
-        res.status(404).json({message: 'Creating Todo failed'})
+        console.log(err);
+        return res.status(404).json({ message: 'Creating Todo failed' })
     }
 
-    res.status(201).json({ todo: createdTodo });
+    return res.status(201).json({ todo: createdTodo });
 };
 
 //get all Todos
 const getTodos = async (req, res, next) => {
     const todos = await Todo.find().exec();
-    res.json(todos);
+    return res.json(todos);
 };
 
 //get Todo by id
 const getTodoById = async (req, res, next) => {
-    const todoId = req.params.tid;
+    const todoId = req.params.todoId;
 
     let todo;
     try {
         todo = await Todo.findById(todoId);
 
     } catch (err) {
-        res.status(404).json({message: 'Todo not found'})
+        return res.status(404).json({ message: 'Todo not found' })
     }
 
     if (!todo) {
-        res.status(404).json({message: 'Could not find Todo for the provided Id'})
+        return res.status(404).json({ message: 'Could not find Todo for the provided Id' })
     }
 
-    res.status(200).json({ todo })
+    return res.status(200).json({ todo })
 
 }
 
-//update todo status
-const updateTodo = async (req, res, next) => {
+//update todo status to isCompleted true 
+const updateTodoCompleted = async (req, res, next) => {
     const { isCompleted } = req.body;
-    const todoId = req.params.tid;
-
+    const todoId = req.params.todoId;
     let todo;
     try {
         todo = await Todo.findById(todoId);
     } catch (err) {
-        res.status(404).json({message: 'Could not find Todo for the provided Id'})
+        res.status(404).json({ message: 'Could not find Todo for the provided Id' })
     }
     todo.isCompleted = isCompleted;
 
     try {
         await todo.save();
     } catch (err) {
-        res.status(404).json({message: 'Could not update Todo'})
+        res.status(404).json({ message: 'Could not update Todo' })
     }
-    res.status(204).json({ message: 'Todo updated'})
+    res.status(204).json({ message: 'Todo updated' })
 }
 
-//delet a todo by id
-const deleteTodo = async (req, res, next) => {
-    const todoId = req.params.tid;
+//update todo details
+const updateTodo = async (req, res, next) => {
+    const { name, reminderAt, reminder } = req.body;
+    const todoId = req.params.todoId;
 
     let todo;
     try {
         todo = await Todo.findById(todoId);
     } catch (err) {
-        res.status(404).json({message: 'Todo not found'})
-        return next(error)
+        return res.status(404).json({ message: 'Could not find Todo for the provided Id' })
+    }
+
+    todo.name = name;
+    todo.reminderAt = reminderAt;
+    todo.reminder = reminder;
+    try {
+        await todo.save();
+    } catch (err) {
+        return res.status(404).json({ message: 'Could not update Todo' })
+    }
+    return res.status(204).json({ message: 'Todo updated' })
+}
+
+//delet a todo by id
+const deleteTodo = async (req, res, next) => {
+    const todoId = req.params.todoId;
+
+    let todo;
+    try {
+        todo = await Todo.findById(todoId);
+    } catch (err) {
+        res.status(404).json({ message: 'Todo not found' })
+
     }
 
     try {
         await todo.remove();
     } catch (err) {
-        res.status(500).json({message: 'Could not Delete Todo'})
-        return next(error);
+        return res.status(500).json({ message: 'Could not Delete Todo' })
+
     }
-    res.status(204).json({ message: 'Deleted todo.' })
+    return res.status(204).json({ message: 'Deleted todo.' })
 }
 
 
@@ -99,4 +122,5 @@ exports.createTodo = createTodo;
 exports.getTodos = getTodos;
 exports.getTodoById = getTodoById;
 exports.updateTodo = updateTodo;
+exports.updateTodoCompleted = updateTodoCompleted;
 exports.deleteTodo = deleteTodo;
--- routes/todos-routes.js
+++ routes/todos-routes.js
@@ -5,20 +5,18 @@ const todoControllers = require('../controllers/todos-controllers')
 const router = express.Router();
 
 router.get('/', todoControllers.getTodos);
-router.get('/:tid', todoControllers.getTodoById);
+router.get('/:todoId', todoControllers.getTodoById);
 router.post('/',
     [
         check('name')
-            .not()
-            .isEmpty(),
-        check('time')
             .not()
             .isEmpty()
     ],
     todoControllers.createTodo
 );
-router.patch('/:tid', todoControllers.updateTodo);
-router.delete('/:tid', todoControllers.deleteTodo)
+router.patch('/:todoId', todoControllers.updateTodo);
+router.patch('/:todoId/completed', todoControllers.updateTodoCompleted);
+router.delete('/:todoId', todoControllers.deleteTodo);
 
 
 
